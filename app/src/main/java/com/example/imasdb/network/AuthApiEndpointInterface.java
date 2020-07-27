543e3258f131b307aa2fb5a37643734669b6b58d
mahdihma <mahdi.haji@yahoo.com>
2020-07-28 02:12:20 +0430

    Authentication Api Endpoint Interface for network retrofit2 completed

--- app/src/main/java/com/example/imasdb/network/AuthApiEndpointInterface.java
+++ app/src/main/java/com/example/imasdb/network/AuthApiEndpointInterface.java
@@ -1,4 +1,22 @@
 package com.example.imasdb.network;
 
+import com.example.imasdb.model.Session;
+import com.example.imasdb.model.Token;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface AuthApiEndpointInterface {
+    @GET("/authentication/token/new")
+    Call<Token> getRequestToken(@Query("api_key") String apiKey);
+
+    @POST
+    Call<Token> getValidateToken(@Query("api_key") String apiKey,
+                                 @Body String username, @Body String password, @Body String requestToken);
+
+    @POST
+    Call<Session> getUserSession(@Query("api_key") String apiKey, @Body String requestToken);
 }
